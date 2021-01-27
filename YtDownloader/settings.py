3dc05ee8bd6a9ebe5cf16a51bd4535cefe880313
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-01-28 00:48:52 +0200

    Update settings.py

--- YtDownloader/settings.py
+++ YtDownloader/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'YtDownloader.urls'
