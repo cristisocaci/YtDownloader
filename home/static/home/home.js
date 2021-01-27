4fd0e555bab8c2facf8261abc3d08bc1c21e50bb
cristisocaci <59095403+cristisocaci@users.noreply.github.com>
2021-01-28 01:26:33 +0200

    styling

--- home/static/home/home.js
+++ home/static/home/home.js
@@ -1,7 +1,13 @@
 function sendLink(type) {
-    document.getElementById('btnleft').hidden=true;
-    document.getElementById('btnright').hidden=true;
+    if(type==='left'){
+        document.getElementById('spinnerright').hidden = true;
+    }
+    else if(type ==='right'){
+        document.getElementById('spinnerleft').hidden = true;
+    }
     document.getElementById('spinner1').style.display = 'flex';
     document.getElementById('spinner2').style.display = 'flex';
+    document.getElementById('btnleft').hidden=true;
+    document.getElementById('btnright').hidden=true;
 }
 
